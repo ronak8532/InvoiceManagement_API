a3589ce7e7f9d7b82bfc73165063b5e3c0d9453b
RD <ronak.dumaniya@gmail.com>
2021-09-03 21:09:36 +0530

    var removed

--- config/var.js
+++ config/var.js
@@ -13,21 +13,7 @@ const config = {
     host: process.env.DB_HOST,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    name: process.env.DB_NAME,
-    dbUrl: process.env.DB_URL,
-  },
-  mail: {
-    username: process.env.MAIL_USERNAME,
-    password: process.env.MAIL_PASSWORD,
-    service: process.env.MAIL_SERVICE,
-    from: process.env.MAIL_FROM,
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    siteLink: process.env.SITE_LINK
-  },
-  razorpay: {
-    id: process.env.RAZOR_PAY_KEY_ID,
-    secret: process.env.RAZOR_PAY_KEY_SECRET,
+    name: process.env.DB_NAME
   }
 };
 
